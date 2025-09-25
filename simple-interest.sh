f594b9c5412e0a22dbd512a97e947996e290ca92
jffgrc <jffgrc.jg@gmail.com>
2025-09-25 11:20:55 -0700

    Update and rename simple-interest.sh.txt to simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+Use the following best practices to structure the content. When adding content to an existing article, follow the existing structure whenever possible.
+
+Provide initial context. Define the topic and state its relevance to the reader.
+Structure the content in a logical order by importance and relevance. Place information in order of priority, and in the order users will need it.
+Avoid long sentences and paragraphs.
+Introduce concepts one by one.
+Use one idea per paragraph.
+Use one idea per sentence.
+Emphasize the most important information.
+Begin each sentence or paragraph with the most important words and takeaways.
+When explaining a concept, start with the conclusion, then explain it in more detail. (This is sometimes called an "inverted pyramid.")
+When explaining a complex topic, present readers with the basic information first, and disclose the details later in the article.
+Use meaningful subheadings. Organize related paragraphs into sections. Give each section a subheading that is unique and that accurately describes the content.
+Consider using in-page links for longer content. This allows readers to jump to areas of interest and skip content that is irrelevant to them.
